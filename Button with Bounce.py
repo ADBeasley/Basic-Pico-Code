1c5aecd4b900d24fd9b4413095758abd626d0ed6
ADBeasley <39886283+ADBeasley@users.noreply.github.com>
2024-05-03 17:10:33 +0100

    Lib and new code
    Added code examples and moved libraries to lib directory

--- Button with Bounce.py
+++ Button with Bounce.py
@@ -2,11 +2,15 @@ from machine import Pin
 from time import sleep
 
 btn = machine.Pin(18, machine.Pin.IN, machine.Pin.PULL_UP)
+btnCount = 0
 
 def btn_handler(pin):
+
+    global btnCount
+    btnCount += 1
     
     if pin is btn:
-        print("Button Pressed")
+        print(f"Button pressed {btnCount:>2}")
         sleep(1)
         
 btn.irq(trigger = machine.Pin.IRQ_RISING, handler = btn_handler)
